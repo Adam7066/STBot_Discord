b2536a5ae5eb3c3add192ae6132e01b87cceb5ee
Adam7066 <adam20001002@gmail.com>
2022-06-26 20:31:28 +0800

    feat: add on_command_error

--- src/event.py
+++ src/event.py
@@ -8,6 +8,15 @@ class event(cogExtension):
         if '小十' in msg.content and msg.author != self.bot.user:
             await msg.channel.send('小十最可愛了!!')
 
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.errors.MissingRequiredArgument):
+            await ctx.send('請輸入正確的參數')
+        elif isinstance(error, commands.errors.CommandNotFound):
+            await ctx.send('OAO 沒有這個指令啦!!')
+        else:
+            await ctx.send(error)
+
 
 def setup(bot):
     bot.add_cog(event(bot))
