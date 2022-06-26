06d143e3695be80fc29eff7283f09ba1bfecc96d
Adam7066 <adam20001002@gmail.com>
2022-06-26 19:04:26 +0800

    feat: add say and clean

--- src/normal.py
+++ src/normal.py
@@ -7,6 +7,15 @@ class normal(cogExtension):
     async def ping(self, ctx):
         await ctx.send(f'Latency: {round(self.bot.latency * 1000, 2)} ms')
 
+    @commands.command()
+    async def say(self, ctx, *, msg):
+        await ctx.message.delete()
+        await ctx.send(msg)
+
+    @commands.command()
+    async def clean(self, ctx, num: int):
+        await ctx.channel.purge(limit=num + 1)
+
 
 def setup(bot):
     bot.add_cog(normal(bot))
