15831245252eee9864f27e8ff22354646d083315
Adam7066 <adam20001002@gmail.com>
2022-06-28 19:44:03 +0800

    fix: repair the problem of dailySignIn

--- src/hoyolab.py
+++ src/hoyolab.py
@@ -10,21 +10,17 @@ from zoneinfo import ZoneInfo
 class hoyolab(cogExtension):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.dailySignInFlag = False
         self.settingJson = tool.getSettingJson()
         self.botTestchannel = self.bot.get_channel(self.settingJson['botTestChannelID'])
+        self.signInDate = self.settingJson['genshinSignInDate']
 
         async def dailySignIn():
             await self.bot.wait_until_ready()
             while not self.bot.is_closed():
-                nowTime = (
-                    datetime.now(tz=timezone.utc)
-                    .astimezone(ZoneInfo('Asia/Taipei'))
-                    .strftime('%H%M')
+                nowTime = datetime.now(tz=timezone.utc).astimezone(
+                    ZoneInfo('Asia/Taipei')
                 )
-                if '0000' <= nowTime <= '0010' and self.dailySignInFlag == True:
-                    self.dailySignInFlag = False
-                elif nowTime >= '0010' and self.dailySignInFlag == False:
+                if nowTime.date() != self.signInDate:
                     for i in self.settingJson['genshin']:
                         cookies = {
                             "ltuid": i['ltuid'],
@@ -40,7 +36,7 @@ class hoyolab(cogExtension):
                             await self.botTestchannel.send(
                                 f"{i['name']} 每日簽到獲得 {reward.amount} x {reward.name}"
                             )
-                    self.dailySignInFlag = True
+                    self.signInDate = nowTime.date()
                 await asyncio.sleep(3600)
 
         self.bgTask = self.bot.loop.create_task(dailySignIn())
